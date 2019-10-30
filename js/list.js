c6bfd751d2b5f7a62e3763a2ed49aadc54781d4d
algerkong <1455048564@qq.com>
2019-10-30 14:28:23 +0800

    交作业

--- js/list.js
+++ js/list.js
@@ -1,10 +1,10 @@
-function showImgAlt() {
-    var imgs = document.getElementById('#imgl');
-    console.log(imgs);
+// function showImgAlt() {
+//     var imgs = document.getElementById('#imgl');
+//     console.log(imgs);
     
-    var img_alt = document.getElementById('#img-alt');
-console.log(img_alt);
+//     var img_alt = document.getElementById('#img-alt');
+// console.log(img_alt);
 
-    img_alt.innerText = imgs.alt;
-    console.log(img_alt.innerText);
-}
+//     img_alt.innerText = imgs.alt;
+//     console.log(img_alt.innerText);
+// }
